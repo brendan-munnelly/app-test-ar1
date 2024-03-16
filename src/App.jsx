d912ee4a836f38e612cba36ce4942437a4a8cfaa
Brendan Munnelly <munnellyb@gmail.com>
2024-03-16 08:25:56 +0000

    Added project files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ListDrivers from './pages/ListDrivers';
+import TableDrivers from './pages/TableDrivers';
+    
+import './F1.css'
+    
+function App() {
+  return (
+    <>
+       <ListDrivers />
+       <TableDrivers />
+    </>
+  );
+}	
+    
+export default App;
